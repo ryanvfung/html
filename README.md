24bcd25cab17cbce251b9c9d6586341e615043a2
Ryan Fung <rvfung@gmail.com>
2015-02-12 15:53:08 +0000

    5.1.001 initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+HTML Template
+=============
+
+Description
+-----------
+
+A starting boilerplate for any website development based on the *PyroCarbon* theme, with essential HTML structure, CSS styling and JavaScript scripting included.
+
+Available for use under the MIT license.
+
+**Author**: Ryan Fung
+
+**Created**: 2010
+
+**Last Modified**: 2015-02-12
+
+
+Version 5.1 Contents
+--------------------
+
+* **z51.html** - HTML File (Core)
+* **z51.css** - CSS File (Core)
+* **z51.js** - JavaScript File (Core)
+* *z51-forms.css* - CSS File for HTML form tags (Optional)
+* *z51-html-ext.css* - CSS File for more HTML tags (Optional)
+* *menu.svg* - SVG image for menu 'hamburger' icon - embedded in **z51.css**
+* *menu.min.svg* - Minimised version of *menu.svg*
---
+++ z51.js
@@ -0,0 +1,48 @@
+/* Ryan Fung's HTML Template v5.1 JavaScript */
+window.onload = function(){
+    $('#navbar-title').text( $('#subtitle').text() );
+    var a = $('nav#nav a');
+    for( i = 0; i < a.length; i++; ) {
+        if( a[i].text == $('#subtitle').text() ) {
+            $(a[i]).addClass('intra nav-this');
+        }
+    }
+    document.title = $('#subtitle').text() + ' | ' + $('#title').text();
+    if( window.location.hash != '' ) {
+        window.setTimeout(
+            function () {
+                if ( $('#navbar').height() == 0 ) {
+                    $.scrollTo($(window.location.hash).offset().top - $('header').height() - 1*$('header').css('paddingTop').split('px')[0] + 'px', 500);
+                } else {
+                    $.scrollTo($(window.location.hash).offset().top - $('#navbar').height() + 'px', 500);
+                }
+            },
+            1000
+        );
+    }
+    $('#navbar-button').click( function () {
+        $('body').toggleClass('nav-active');
+    });
+    $('nav#nav > ul > li a').click( function () {
+        $('body').toggleClass('nav-active');
+    });
+    $('nav#nav .intra a').click( function () {
+        if ( $('#navbar').height() == 0 ) {
+            $.scrollTo($('#'+this.href.split('#')[1]).offset().top - $('header').height() - 1*$('header').css('paddingTop').split('px')[0] + 'px', 500);
+        } else {
+            $.scrollTo($('#'+this.href.split('#')[1]).offset().top - $('#navbar').height() + 'px', 500);
+        }
+    });
+    $('#content-wrapper').click ( function () {
+        if( $('body').hasClass('nav-active') ) {
+            $('body').toggleClass('nav-active');
+        }
+    });
+}
+$(window).scroll( function () {
+    if( $('body').scrollTop() > $('#header-wrapper').css('paddingTop').split('px')[0] ) { 
+        $('body').addClass('obscured');
+    } else {
+        $('body').removeClass('obscured');
+    }
+});
