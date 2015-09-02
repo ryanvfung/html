91c2fd90321ea70e0ade03ade700a28354f92ad5
Ryan Fung <rvfung@gmail.com>
2015-09-02 22:01:13 +0100

    5.2 Move scrollspy to JavaScript, move code section, update links, update styles

--- 5.2/z52.js
+++ 5.2/z52.js
@@ -1,7 +1,13 @@
-$(window).scroll( function () {
-	if ( $("body").scrollTop() > $("main").position().top - 64 ) { // - 64
-		$("#sidenav").addClass("affix")
-	} else {
-		$("#sidenav").removeClass("affix");
-	}
+// $(window).scroll( function () {
+// 	if ( $("body").scrollTop() > $("main").position().top - 64 ) { // - 64
+// 		$("#sidenav").addClass("affix")
+// 	} else {
+// 		$("#sidenav").removeClass("affix");
+// 	}
+// });
+$(document).ready(function(){
+	$('body').scrollspy({ target: '#sidenav', offset: 64 });
+	$('nav#sidenav a').click( function () {
+		$.scrollTo($('#'+this.href.split('#')[1]).offset().top - 64 + 'px', 400);
+	});
 });
